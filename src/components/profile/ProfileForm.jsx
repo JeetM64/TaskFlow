d35b353b2bfed515fc700e424e42214b98de2a49
JeetM64 <jeetmhatre58@gmail.com>
2026-05-31 23:46:41 +0530

    zod validation updated

--- src/components/profile/ProfileForm.jsx
+++ src/components/profile/ProfileForm.jsx
@@ -1,22 +1,47 @@
-
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { profileSchema } from "./profileSchema";
 
 import './profileform.css';
 
 function ProfileForm() {
 
+  // object created (useform to collect data)
+  const {register,handleSubmit,formState: {errors} } = useForm({resolver : zodResolver(profileSchema)});
+
+  function onSubmit(data){
+    console.log(data);
+  }
+
     
   return (
     <div>
       <div className="form-container">
-        <h2>profile Setting </h2>
-        
-        <form className="parent">
-            <input type="text" placeholder="Name"/>
-            <input type = "email" placeholder="Email"/>
-            <input type ="password" placeholder="Password"/>
-            <input type ="password" placeholder="conform Password"/>
-            <button type="submit"> Save Changes </button>
+        <h2>Profile Setting</h2>
 
+        <form className="parent" onSubmit = {handleSubmit(onSubmit)}>
+          <label>
+            Role
+            <select {...register("role")}>
+              <option value="student">Student</option>
+              <option value="developer">Developer</option>
+              <option value="designer">Designer</option>
+              <option value="other">Other</option>
+            </select>
+
+            <p>{errors.role?.message}</p>
+          </label>
+
+          <label>
+            Daily Hours
+            <input type="number" min="0" max="24" placeholder="Hours per day" {...register("goalHours")}/> <p>{errors.goalHours?.message}</p>
+          </label>
+
+          <input type="text" placeholder="Name" {...register("name")}/> <p>{errors.name?.message}</p>
+          <input type="email" placeholder="Email" {...register("email")}/> <p>{errors.email?.message}</p>
+          <input type="password" placeholder="Password" {...register("password")}/> <p>{errors.password?.message}</p>
+          <input type="password" placeholder="Confirm Password" {...register("confirmPassword")}/> <p>{errors.confirmPassword?.message}</p>
+          <button type="submit">Save Changes</button>
         </form>
 
       </div>
---
+++ src/components/profile/profileSchema.js
@@ -0,0 +1,27 @@
+import { z } from "zod";
+
+export const profileSchema = z.object({
+
+    name : z.string()
+          .min(2,"Name should be at least 2 char")
+          .max(50,"Name should be less than 50 char"),
+    email : z.string()
+          .email("Invalid email adress"),
+    password : z.string()
+          .min(6,"Password should be minimum 6 character")
+          .regex(
+          /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/,
+          "Password must contain uppercase, lowercase and a number"
+          ),
+    confirmPassword : z.string()
+          .min(8,"Password should be minimum 8 character"),
+    role : z.enum(["student","developer","designer","other"],{message : "Please select from the options"}) ,
+    goalHours : z.coerce.number()
+           .min(1,"should be at least 1 hr")
+           .max(24,"should be less than 24 hr")
+
+    .refine((data) => data.password === data.confirmPassword, {
+        message: "Passwords do not match",
+        path: ["confirmPassword"]
+    })
+});
